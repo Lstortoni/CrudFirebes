7b5b4bfcc25c4397fa5c38d797418a557cdae48c
Leonardo Stortoni <leodaniel1978@gmail.com>
2022-08-09 10:31:14 -0300

    Repositorio

--- src/LeerFotografos.jsx
+++ src/LeerFotografos.jsx
@@ -28,7 +28,7 @@ const LeerFotografos = () => {
      obteberDatos()   //ejecuto la funcion de inmediato para leer los fotografos .
 
    }, [])
-    
+         
 
   return (
     <div className='container mt-3'>
